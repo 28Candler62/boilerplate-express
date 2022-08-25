b055e324fd4aa34ac1168061d0c61c82cab030ba
John <jcandler2862@gmail.com>
2022-08-24 22:59:41 -0500

    commit

--- myApp.js
+++ myApp.js
@@ -1,18 +1,19 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
-console.log("Hello World"),
+console.log("Hello World");
+absolutePath = __dirname + "/views/index.html";
 
-app.get(/, function(req, res){
-  res.send("Hello World"),
-})
+app.use("/public", express.static( __dirname + "/public"));
 
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/views/index.html");
+});
 
-
-
-
-
-
+app.get("/json", function(req, res){
+  res.json({message: process.env.MESSAGE_STYLE == "uppercase" ? "HELLO JSON" : "Hello json"})
+});
 
 
 
